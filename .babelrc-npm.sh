405eeef38ba6877de8ff3a9709aa4a847382889f
COSWARS <1850214353@qq.com>
2024-12-06 02:12:19 +0800

    2024/12/06 周五 2:12:19.22

---
+++ .babelrc-npm.sh
@@ -0,0 +1,8 @@
+# 必备: @babel/core
+## TS
+- @babel/plugin-transform-typescript
+- babel-plugin-const-enum #枚举类型
+- babel-plugin-replacets-export-assignment #Export=Import=
+# 管道符：
+- @babel/plugin-proposal-pipeline-operator
+- @babel/plugin-transform-regenerator
